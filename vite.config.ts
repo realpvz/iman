6947a06f104dfae9efddc06bceb3a271f6016747
Iman Parvizi <70389233+realpvz@users.noreply.github.com>
2025-03-10 17:57:35 +0000

    gg

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [
     react(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/iman",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  base: '/iman'
+  }
 }));
